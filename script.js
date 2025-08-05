3ef85a6c7c1d1023ef259e8cf16a4892ad289a8f
Amalia Prawinda <amalia.a.prawinda@gmail.com>
2025-08-05 16:10:55 +0700

    palindrome check function

--- script.js
+++ script.js
@@ -2,4 +2,22 @@ document.getElementById('check-btn').addEventListener('click', function(e) {
     e.preventDefault();
 
     const inputText = document.getElementById('text-input').value;
-})
+    const result = document.getElementById('result');
+
+    if (!inputText) {
+        alert("Please input a value.");
+        return;
+    }
+
+
+    const processedText = inputText.replace(/[^a-z0-9]/gi, '').toLowerCase();
+    const reversedText = processedText.split('').reverse().join('');
+
+    if (processedText === reversedText) {
+        result.textContent = `${inputText} is a palindrome!`;
+    } else {
+        result.textContent = `${inputText} is not a palindrome.`;
+    }
+
+
+});
